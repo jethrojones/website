97215051df436970b7a4e310a3009ffca25f0e4e
Jethro Jones <jethrojones+git@gmail.com>
2025-01-07 11:38:23 -0800

    Updated Category generator to fix display name issue

--- _plugins/category_generator.rb
+++ _plugins/category_generator.rb
@@ -7,17 +7,20 @@ module Jekyll
         # Generate slug for the category
         slug = category.downcase.gsub(' ', '-').gsub(/[^\w-]/, '')
 
+        # Capitalize the first letter for display purposes
+        display_name = category.split.map(&:capitalize).join(' ')
+
         # Debugging: Output the category and its generated slug
         puts "Generating category '#{category}' with slug '#{slug}'"
 
         # Create a new category page
-        site.pages << CategoryPage.new(site, site.source, category, slug, posts)
+        site.pages << CategoryPage.new(site, site.source, display_name, slug, posts)
       end
     end
   end
 
   class CategoryPage < Page
-    def initialize(site, base, category, slug, posts)
+    def initialize(site, base, display_name, slug, posts)
       @site = site
       @base = base
       @dir  = "categories/#{slug}" # Use the slug for the directory name
@@ -25,20 +28,27 @@ module Jekyll
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'category.html')
-      self.data['category'] = category
+      self.data['category'] = display_name
       self.data['slug'] = slug
       self.data['posts'] = posts
-      self.data['title'] = "Category: #{category.capitalize}"
-        # RSS feed
-      create_feed_page(site, base, display_name, slug, docs)
-  	end
+      self.data['title'] = "Category: #{display_name}"
 
-  	def create_feed_page(site, base, display_name, slug, docs)
-    feed = Page.new(site, base, "categories/#{slug}", "feed.xml")
-    feed.process("feed.xml")
-    feed.content = File.read(File.join(base, "_feed_category.xml"))
-    feed.data['category'] = display_name.downcase
-    site.pages << feed
- 	end
+      # Create RSS feed
+      create_feed_page(site, base, display_name, slug, posts)
+    end
+
+    def create_feed_page(site, base, display_name, slug, posts)
+      feed = Page.new(site, base, "categories/#{slug}", "feed.xml")
+      feed.process("feed.xml")
+      
+      # Read and set content for the RSS feed
+      feed.content = File.read(File.join(base, "_feed_category.xml"))
+      feed.data['category'] = display_name
+      feed.data['slug'] = slug
+      feed.data['posts'] = posts
+
+      # Add to site pages
+      site.pages << feed
     end
   end
+end
