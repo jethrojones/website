545998990daffaf2645f0b84ac44acd7183d6e6a
jethrojones <jethrojones@gmail.com>
2025-06-07 22:19:15 -0700

    Fix pagination: process Markdown on generated pages

--- _plugins/notes_pagination_generator.rb
+++ _plugins/notes_pagination_generator.rb
@@ -4,7 +4,7 @@ module Jekyll
       @site = site
       @base = base
       @dir  = dir
-      @name = 'index.html'
+      @name = index.name
 
       process(@name)
       self.content = index.content
